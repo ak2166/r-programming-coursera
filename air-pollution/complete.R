5e432e9c1a42ab9260727f0522f1bec448d49de4
Anthony A. Kolodzinski <anthonykolodzinski@gmail.com>
2016-08-26 09:44:42 -0400

    Refactor complete and corr functions

--- air-pollution/complete.R
+++ air-pollution/complete.R
@@ -1,12 +1,19 @@
 complete <- function(directory, id = 1:332){
     files_queried <- list.files(directory, pattern="*.csv", full.names=TRUE)[id]
+    getcompletes(files_queried, id)
+}
+
+getcompletes <- function(files, id){
     completes <- data.frame()
-    for (file in files_queried){
-        df <- read.csv(file)
-        complete_vec = 	complete.cases(df)   
-        row <- cbind (id[match(file, files_queried)], sum(complete_vec, na.rm=TRUE))
+    for (file in files){
+        row <- completerow(file, files, id)
         completes <- rbind(completes, row)
     }
     colnames(completes) <- c("id", "nobs")
     completes
+}
+
+completerow <- function(file, files, id){
+    complete_vec = 	complete.cases(read.csv(file))   
+    cbind (id[match(file, files)], sum(complete_vec, na.rm=TRUE))
 }
--- air-pollution/corr.R
+++ air-pollution/corr.R
@@ -1,14 +1,21 @@
 source("complete.R")
+
 corr <- function(directory, threshold = 0){
+    files_queried <- getcompleteframe(directory, threshold)
+    calculatecors(files_queried, 'sulfate', 'nitrate')
+}
+
+getcompleteframe <- function(directory, threshold){
     completes <- complete(directory)
+    list.files(directory, pattern="*.csv", full.names=TRUE)[completes[(completes['nobs'] > threshold),"id"]]
+}
+
+calculatecors <- function(files, pollutant_one, pollutant_two){
     cors <- numeric()
-    for(case in 1:nrow(completes)){
-        if(completes[case, 'nobs'] > threshold){
-            file <- list.files(directory, pattern="*.csv", full.names=TRUE)[completes[case, 'id']]
-            df <- read.csv(file)
-            clean_df <- df[complete.cases(df),]
-            cors <- c(cors, cor(clean_df['sulfate'], clean_df['nitrate']))
-        }
+    for(file in files){
+        df <- read.csv(file)
+        clean_df <- df[complete.cases(df),]
+        cors <- c(cors, cor(clean_df[pollutant_one], clean_df[pollutant_two]))
     }
     cors
 }
